804379e3e7d04d30ecf49eb65bba3e7e9c212b37
Gyotek <a.herbreteau@rubika-edu.com>
2020-09-26 01:00:19 +0200

    Update unity project, test odin

---
+++ Assets/Scripts/ARH_Test1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class ARH_Test1 : SerializedMonoBehaviour
+{
+    [Button]
+    void TestLog() => Debug.Log("Test succed");
+    private enum testEnum {one, two, chocolat };
+    [SerializeField] private Dictionary<testEnum, string> testDico;
+
+}
