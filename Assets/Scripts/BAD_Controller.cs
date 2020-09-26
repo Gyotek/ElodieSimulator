67634820f65527d0ab46ba2801141b79aac65c52
Ryo207 <b.decarvalho@rubika-edu.com>
2020-09-26 19:25:03 +0200

    Merge du Controller et de l'UI

--- Assets/Scripts/BAD_Controller.cs
+++ Assets/Scripts/BAD_Controller.cs
@@ -8,10 +8,17 @@ public class BAD_Controller : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    private object firePoint;
 
-    //Variable Tir 
-    public Camera cam;
-    Vector2 mousePos;
+    public Transform SignPoint;
+    public GameObject Green_Sign;
+    public GameObject Red_Sign;
+    public bool closeToSign;
+    private GameObject CloseSign = null;
+
+    private int MaxSign = 4;
+    private int TotalSign;
+
 
     void Start()
     {
@@ -23,19 +30,86 @@ public class BAD_Controller : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
 
+            if (closeToSign)
+            {
+                Destroy(CloseSign);
+                TotalSign--;
+            }
+            else if (MaxSign > TotalSign)
+            {
+                SetSign(BAD_Sign.SignTypes.Green);
+                TotalSign++;
+            }
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+
+        {
+            if (closeToSign)
+            {
+                Destroy(CloseSign);
+                TotalSign--;
+            }
+            else if (MaxSign > TotalSign)
+            {
+                SetSign(BAD_Sign.SignTypes.Red);
+                TotalSign++;
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        //rb.MovePosition(rb.position + movement * moveSpeed );
-
         rb.velocity = new Vector2(movement.x, movement.y).normalized * moveSpeed;
-
-        Vector2 lookDirection = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
     }
+
+    void SetSign(BAD_Sign.SignTypes _signType)
+    {
+        GameObject SignPrefab = null;
+        switch (_signType)
+        {
+            case BAD_Sign.SignTypes.Green:
+                SignPrefab = Green_Sign;
+                break;
+            case BAD_Sign.SignTypes.Red:
+                SignPrefab = Red_Sign;
+                break;
+            default:
+                break;
+        }
+
+        GameObject _SignPrefab = Instantiate(SignPrefab, SignPoint.position, SignPoint.rotation);
+        Rigidbody2D rb = _SignPrefab.GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BAD_Sign>() && closeToSign != true)
+        {
+            closeToSign = true;
+            CloseSign = collision.gameObject;
+
+        }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BAD_Sign>())
+        {
+            closeToSign = false;
+            CloseSign = null;
+          
+        }
+
+    }
+
+
+
+
 }
